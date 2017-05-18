9be5f303b2c211957edf0b4c8e474d2a9ec4ca6b
Robert Roubos <robertroubos9@gmail.com>
2017-05-18 11:46:30 +0200

    as

---
+++ Shared/src/main/java/library/Lobby.java
@@ -0,0 +1,13 @@
+package library;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface Lobby extends Remote {
+	
+	public List<ServerGame> lookupGame(String name, int limit) throws RemoteException;
+	
+	public ServerGame addGame(String name) throws RemoteException;
+	
+}
